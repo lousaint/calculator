975b4fdb34bcd0de0f7e4155978d6314c3d292e1
Louis St. Hilaire <lousaint@hotmail.com>
2020-02-11 20:54:55 -0500

    Add basic operator functions

---
+++ scripts/calculator.js
@@ -0,0 +1,23 @@
+function operate(a, b, func) {
+    return func(a, b);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        return "ERR";
+    }
+}
